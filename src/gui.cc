eb76428034554de06e7c83f6f06d4b5bdb40999f
imShahriar-klvd <imshahriar.klvd@gmail.com>
2025-07-24 16:56:45 +0330

    set right location

--- src/gui.cc
+++ src/gui.cc
@@ -39,7 +39,7 @@ void Gui::run() {
         DrawTexturePro(gameMap, Src, Dest, origin, 0 , WHITE);
 
         drawLeftPanel();
-        drawRightPanel();
+        //drawRightPanel();
 
         handleInput();
 
